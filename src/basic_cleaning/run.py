eb5e91c1b68657b577cdea460905858a820684f2
ed04alb@gmail.com <ed04alb@gmail.com>
2021-12-09 15:46:21 -0600

    fix test boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,6 +36,10 @@ def go(args):
     logger.info(f"Converting last_review column to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("drop rows in the dataset that are not in the proper geolocation")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info(f"Saving the dataset")
     df.to_csv("clean_sample.csv", index=False)
 
